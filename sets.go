5221913eb84ac279c18c55369153aa09dd3419dc
Philip K <philip@warpmail.net>
2018-09-05 23:26:19 +0200

    Stabilized sets

--- sets.go
+++ sets.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
@@ -12,61 +11,64 @@ import (
 type Set []string
 
 var (
-	setDir  string
-	sets    map[string]Set
-	setLock sync.Mutex
+	sets     map[string]Set
+	setNames []string
+	setLock  sync.Mutex
 )
 
-func (p *Room) toggleSet(setName string) (loaded bool, err error) {
+func (p *Room) toggleSet(setName string) error {
 	set, ok := sets[setName]
 	if !ok {
-		err = fmt.Errorf("No such set %q", setName)
-		return
+		return nil
 	}
 
 	wd, err := os.Getwd()
 	if err != nil {
-		return
+		return err
 	}
 
-	p.Lock()
-	defer p.Unlock()
+	// p.Lock()
 	defer p.loadVideos()
+	// defer p.Unlock()
 
-	if _, loaded = p.Sets[setName]; ok { // unload
+	_, loaded := p.Sets[setName]
+	log.Printf("Toggled set (%v) %q in %q", loaded, setName, p.Key)
+
+	if !loaded { // load
 		for _, v := range set {
 			err = os.Symlink(v, path.Join(wd, p.Key, path.Base(v)))
 			if err != nil {
-				return
-			}
-		}
-		delete(p.Sets, setName)
-		return
-	} else {
-		for _, v := range set {
-			err = os.Remove(path.Join(wd, p.Key, path.Base(v)))
-			if err != nil {
-				return
+				log.Println(err)
+				return err
 			}
 		}
 		p.Sets[setName] = len(set)
-		return
+	} else { // unload
+		for _, v := range set {
+			file := path.Join(wd, p.Key, path.Base(v))
+			if _, err = os.Stat(file); err == nil && file != p.Watching {
+				err = os.Remove(file)
+				if err != nil {
+					log.Println(err)
+					return err
+				}
+			}
+		}
+		delete(p.Sets, setName)
 	}
+	return nil
 }
 
-func loadSets() {
-	if setDir == "" {
-		return
-	}
-
+func loadSets(setDir string) error {
 	setLock.Lock()
 	defer setLock.Unlock()
 	sets = make(map[string]Set)
+	setNames = nil
 
+	log.Println("Loading sets from ", setDir)
 	setDirs, err := ioutil.ReadDir(setDir)
 	if err != nil {
-		log.Println(err)
-		return
+		return err
 	}
 
 	for _, set := range setDirs {
@@ -75,13 +77,16 @@ func loadSets() {
 
 		s, err := ioutil.ReadDir(path.Join(setDir, name))
 		if err != nil {
-			log.Println(err)
-			return
+			return err
 		}
 
 		for _, vid := range s {
-			sets[name] = append(sets[name],
-				path.Join(setDir, name, vid.Name()))
+			sets[name] = append(sets[name], path.Join(setDir, name, vid.Name()))
 		}
+
+		setNames = append(setNames, name)
+		log.Printf("Added set %q with %d items", name, len(sets[name]))
 	}
+
+	return nil
 }
