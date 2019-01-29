a84c9e1c8d3ff4dd63a42327eb10446c682c0d55
Philip K <philip@warpmail.net>
2019-01-29 22:42:07 +0100

    Updated User struct

--- user.go
+++ user.go
@@ -4,13 +4,15 @@ import ws "github.com/gorilla/websocket"
 
 // User represents, and contains, a websocket connection
 type User struct {
-	id      uint32
-	key     string
-	conn    *ws.Conn
-	ignored bool
-	next    bool
-	ready   bool
-	msg     chan<- Msg
+	ID   uint32 `json:"id"`   // internal ID for a user per room
+	Name string `json:"name"` // public username
+
+	next  bool       // has finished watching current video
+	ready bool       // is ready to play next video
+	oper  bool       // is user an operator
+	key   string     // verification key
+	conn  *ws.Conn   // socket connection for a user
+	msg   chan<- Msg // channel for sending messages to this user
 }
 
 func (u *User) talker() {
